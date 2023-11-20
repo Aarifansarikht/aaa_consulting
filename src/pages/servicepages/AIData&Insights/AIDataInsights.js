fa0bd3b2ecd6ec2b5bff080200fd02ca70d4eb32
Aarif Ansari <86506816+Aarifansarikht@users.noreply.github.com>
2023-11-20 15:43:02 +0530

    content

--- src/pages/servicepages/AIData&Insights/AIDataInsights.js
+++ src/pages/servicepages/AIData&Insights/AIDataInsights.js
@@ -49,90 +49,85 @@ function AIDataInsights(props) {
       </section>
 
       <section>
-        <article>
-          <div className={Style.Container}>
-            <div className={Style.TextContent}>
-              <h1>Unlocking Infinite Possibilities</h1>
-              <p>
-                Your data is your most valuable asset — holding real potential
-                to revolutionize what your business can achieve. But without a
-                modern solution to unify, process and deliver your analytics,
-                you risk missing out on key information to innovate, adapt and
-                grow.
-              </p>
-              <p>
-                We’ll assess your current data landscape, modernize existing
-                solutions and identify high-value opportunities to use data and
-                AI in your organization to consistently deliver success. Through
-                a defined strategy and end-to-end capabilities, we establish the
-                processes and tools to meet your business objectives.
-              </p>
+        <div className={Style.Container}>
+          <div className={Style.TextContent}>
+            <h1>Unlocking Infinite Possibilities</h1>
+            <p>
+              Your data is your most valuable asset — holding real potential to
+              revolutionize what your business can achieve. But without a modern
+              solution to unify, process and deliver your analytics, you risk
+              missing out on key information to innovate, adapt and grow.
+            </p>
+            <p>
+              We’ll assess your current data landscape, modernize existing
+              solutions and identify high-value opportunities to use data and AI
+              in your organization to consistently deliver success. Through a
+              defined strategy and end-to-end capabilities, we establish the
+              processes and tools to meet your business objectives.
+            </p>
 
-              <h1>86%</h1>
-              <p>
-                of organizations have already adopted or are considering
-                data-driven projects.*
-              </p>
-              <h1>64%</h1>
-              <p>
-                of leaders agree that data collection and analysis changed how
-                they’ve conducted business in the past three years.*
-              </p>
-              <h1>39%</h1>
-              <p>
-                of IT decision-makers report that data and business analytics
-                would receive the most IT investment in their organization.*
-              </p>
-            </div>
+            <h1>86%</h1>
+            <p>
+              of organizations have already adopted or are considering
+              data-driven projects.*
+            </p>
+            <h1>64%</h1>
+            <p>
+              of leaders agree that data collection and analysis changed how
+              they’ve conducted business in the past three years.*
+            </p>
+            <h1>39%</h1>
+            <p>
+              of IT decision-makers report that data and business analytics
+              would receive the most IT investment in their organization.*
+            </p>
           </div>
-        </article>
+        </div>
       </section>
 
       <section>
-        <article>
-          <div className={Style.TextContent}>
-            <div className={Style.Container}>
-              <h1 style={{ paddingBottom: "2rem" }}>
-                Meaningful outcomes delivered through data
-              </h1>
-              <div className={Style.CardsContainer}>
-                <ContentCard
-                  heading="Maximize the value of your data assets."
-                  pera="Modern platforms collect, analyze and manage data from diverse sources to harness untapped business potential and grow return on investment."
-                  image={MaximizeValue}
-                />
-                <ContentCard
-                  heading="Quickly innovate products and services at scale."
-                  pera="Predictive analytics and AI-powered trend forecasting enables you to identify new business opportunities and gain a competitive advantage."
-                  image={ServicesAtScale}
-                />
-                <ContentCard
-                  heading="Improve customer experiences."
-                  pera="Processing customer data allows you to automate inventory management, better understand purchasing decisions and market to your audience more effectively."
-                  image={customersatisfration}
-                />
-                <ContentCard
-                  heading="Reduce repetitive tasks.
+        <div className={Style.TextContent}>
+          <div className={Style.Container}>
+            <h1 style={{ paddingBottom: "2rem" }}>
+              Meaningful outcomes delivered through data
+            </h1>
+            <div className={Style.CardsContainer}>
+              <ContentCard
+                heading="Maximize the value of your data assets."
+                pera="Modern platforms collect, analyze and manage data from diverse sources to harness untapped business potential and grow return on investment."
+                image={MaximizeValue}
+              />
+              <ContentCard
+                heading="Quickly innovate products and services at scale."
+                pera="Predictive analytics and AI-powered trend forecasting enables you to identify new business opportunities and gain a competitive advantage."
+                image={ServicesAtScale}
+              />
+              <ContentCard
+                heading="Improve customer experiences."
+                pera="Processing customer data allows you to automate inventory management, better understand purchasing decisions and market to your audience more effectively."
+                image={customersatisfration}
+              />
+              <ContentCard
+                heading="Reduce repetitive tasks.
                   "
-                  pera="Automation solutions fill workflow gaps and enhance productivity — so your team can focus on high-value tasks."
-                  image={ReduceRepeattask}
-                />
-                <ContentCard
-                  heading="Improve efficiency and reduce human error."
-                  pera={
-                    "Analytics reveal opportunities to optimize processes and boost performance, all while making your operations more reliable."
-                  }
-                  image={ImproveEfficency}
-                />
-                <ContentCard
-                  heading="Make more informed business decisions."
-                  pera="Business intelligence solutions provide a comprehensive view of data to answer critical questions and determine the next steps to meet your needs."
-                  image={MakeMoreDecision}
-                />
-              </div>
+                pera="Automation solutions fill workflow gaps and enhance productivity — so your team can focus on high-value tasks."
+                image={ReduceRepeattask}
+              />
+              <ContentCard
+                heading="Improve efficiency and reduce human error."
+                pera={
+                  "Analytics reveal opportunities to optimize processes and boost performance, all while making your operations more reliable."
+                }
+                image={ImproveEfficency}
+              />
+              <ContentCard
+                heading="Make more informed business decisions."
+                pera="Business intelligence solutions provide a comprehensive view of data to answer critical questions and determine the next steps to meet your needs."
+                image={MakeMoreDecision}
+              />
             </div>
           </div>
-        </article>
+        </div>
       </section>
 
       <section>
